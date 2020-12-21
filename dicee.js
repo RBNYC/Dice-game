e3904163533b0dd6c52c0c409af91d8c53002fec
Rachida B <rachida.benyoucef@gmail.com>
2020-12-21 15:39:09 +0100

    beautify code

--- dicee.js
+++ dicee.js
@@ -1,20 +1,18 @@
-var randomNumber1= (Math.random()*6)+1;
+var randomNumber1 = (Math.random() * 6) + 1;
 var randomDice1 = Math.trunc(randomNumber1);
-var imgSrc1 = "images/dice"+randomDice1+".png";
+var imgSrc1 = "images/dice" + randomDice1 + ".png";
 
-var randomNumber2= (Math.random()*6)+1;
+var randomNumber2 = (Math.random() * 6) + 1;
 var randomDice2 = Math.trunc(randomNumber2);
-var imgSrc2 = "images/dice"+randomDice2+".png";
+var imgSrc2 = "images/dice" + randomDice2 + ".png";
 
 document.querySelector(".img1").setAttribute("src", imgSrc1);
 document.querySelector(".img2").setAttribute("src", imgSrc2);
 
-if (randomDice1>randomDice2){
-  document.querySelector(".container h1").innerText="🚩 Player 1 wins";
-}
-else if(randomDice1<randomDice2){
-  document.querySelector(".container h1").innerText="Player 2 wins 🚩";
-}
-else{
-  document.querySelector(".container h1").innerText="Draw again!";
+if (randomDice1 > randomDice2) {
+    document.querySelector(".container h1").innerText = "🚩 Player 1 wins";
+} else if (randomDice1 < randomDice2) {
+    document.querySelector(".container h1").innerText = "Player 2 wins 🚩";
+} else {
+    document.querySelector(".container h1").innerText = "Draw again!";
 }
